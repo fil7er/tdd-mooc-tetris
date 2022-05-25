ccb0c92c10fb72542a0c3040e530d8b3197d20b2
fil7er <fil7er@gmail.com>
2022-05-25 10:33:03 -0400

    Update Board.mjs

--- src/Board.mjs
+++ src/Board.mjs
@@ -15,7 +15,7 @@ export class Board {
     for (let i = 0; i < this.height; i++) {
       for(let i2 = 0; i2 < this.width; i2++){
        this.falling && i2 == 1 && i== 0 ? this.display = this.display+'X': 
-       this.falling && i2 > 1 && i > 0 ? this.display = this.display+'X' : this.display = this.display+'.';
+        this.falling && i2 > 1 && i > 0 ? this.display = this.display+'X' : this.display = this.display+'.';
       }
     this.display = this.display+'\n';}
     return this.display;
