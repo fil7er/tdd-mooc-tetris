3750bd96f047632bcff13c420077e9f4379f4e7b
fil7er <fil7er@gmail.com>
2022-05-25 18:11:17 -0400

    Update Board.mjs

--- src/Board.mjs
+++ src/Board.mjs
@@ -13,14 +13,14 @@ export class Board {
     this.height = height;
   }
 
-  tick(){this.fallingBlockLocation++;}
+  tick(){this.fallingBlockLocation == 1;}
 
   toString() {
     for (let i = 0; i < this.height; i++) {
       for(let i2 = 0; i2 < this.width; i2++){
        this.falling && i2 == 1 && i== 0 && this.fallingBlockLocation == i ? this.display += this.fallingBlock: 
         this.falling && i2 == 2 && i == 1 && this.fallingBlockLocation == i ? this.display += this.fallingBlock : 
-          this.falling && i2 == 2 && i == 2 && this.fallingBlockLocation == i ? this.display += this.fallingBlock : this.display += '.';
+          this.falling && i2 == 2 && i == 2 && this.fallingBlockLocation == i ? this.display += this.fallingBlock : this.display += ',';
       }
     this.display = this.display+'\n';}
     return this.display;
