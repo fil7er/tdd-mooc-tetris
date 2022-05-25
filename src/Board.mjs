f2a3c48ee1b27c41244b1973dd49f00226935926
fil7er <fil7er@gmail.com>
2022-05-25 10:11:55 -0400

    Update Board.mjs

--- src/Board.mjs
+++ src/Board.mjs
@@ -10,7 +10,7 @@ export class Board {
   }
 
   toString() {
-    let display;
+    let display = '';
     for (let i = 0; i < this.height; i++) {
       for(let i2 = 0; i2 < this.width; i2++){
         display = display+'.';
