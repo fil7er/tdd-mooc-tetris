59ed73975dc300e9f6275135e2a8de3cee9a10af
fil7er <fil7er@gmail.com>
2022-06-09 17:40:36 -0400

    pass

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -3,5 +3,6 @@ import {RotatingShape} from './RotatingShape.mjs';
 export class Tetromino {
 
  static T_SHAPE = new RotatingShape(".T."+"\n"+"TTT"+"\n"+"..."+"\n");
+ static I_SHAPE = new RotatingShape("....."+"\n"+"....."+"\n"+"IIII."+"\n"+"....."+"\n"+"....."+"\n");
 
 }
