f68e98510498f5dffdae5f05a2f1cb344dd014b9
fil7er <fil7er@gmail.com>
2022-06-09 16:18:21 -0400

    Making pass, reutilizing other class

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -1,9 +1,11 @@
+import {RotatingShape} from './RotatingShape.mjs';
+
 export class Tetromino {
 
- static T_SHAPE = ".T."+"\n"+"TTT"+"\n"+"..."+"\n";
-
+ static T_SHAPE = new RotatingShape(".T."+"\n"+"TTT"+"\n"+"..."+"\n");
 
  rotateRight(){
+    this.makeArray(this.string);
      return ".T."+"\n"+"T.T"+"\n"+".T."+"\n";
  }
  
