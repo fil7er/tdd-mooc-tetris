8c2b1061b833d39a4bb22950e0c8e693191fe409
fil7er <fil7er@gmail.com>
2022-06-09 17:36:05 -0400

    yes pass

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -7,7 +7,7 @@ export class RotatingShape{
         this.shape = Array.from(Array(row.length), () => new Array(row.length));
         switch(where){
             case 1 :  for(let y= 0;y < this.shape.length;y++)for(let x=this.shape.length-1;x>=0;x--) this.shape[y][(this.shape.length-1-x)] = Array.from(row[x])[y]; break; //Right
-            case 2 : for(let y =this.shape.length-1;y>=0;y--) for(let x=0;x<this.shape.length;x++) this.shape[this.shape.length-1-y][x] = this.shape[x][y]; break; //Left
+            case 2 : for(let y =this.shape.length-1;y>=0;y--) for(let x=0;x<this.shape.length;x++) this.shape[this.shape.length-1-y][x] = Array.from(row[x])[y]; break; //Left
             default : for(let y=0;y< this.shape.length;y++) for(let x=0;x< this.shape.length;x++) this.shape[y][x] = Array.from(row[y])[x]; break;
         }
     }
@@ -17,9 +17,7 @@ export class RotatingShape{
     }
 
     rotateLeft(){
-        let rotated = Array.from(Array(this.shape.length), () => new Array(this.shape.length));
-        for(let y =this.shape.length-1;y>=0;y--) for(let x=0;x<this.shape.length;x++) rotated[this.shape.length-1-y][x] = this.shape[x][y];
-        return (rotated.join("\n").trim().toString()).split(",").join("").trim().toString() + '\n';
+        return new RotatingShape(((this.shape.join("\n").trim().toString()).split(",").join("").trim().toString() + '\n'), 2);
     }
 
     toString(){    
