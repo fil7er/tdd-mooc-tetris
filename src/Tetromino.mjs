f1144788e57c395060a6789109d0fd4035584e78
fil7er <fil7er@gmail.com>
2022-06-09 17:36:30 -0400

    refac

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -4,9 +4,4 @@ export class Tetromino {
 
  static T_SHAPE = new RotatingShape(".T."+"\n"+"TTT"+"\n"+"..."+"\n");
 
- rotateRight(){
-    this.makeArray(this.string);
-     return ".T."+"\n"+"T.T"+"\n"+".T."+"\n";
- }
- 
 }
