98fb02633a49da572341029c9a78ddaa76858a67
fil7er <fil7er@gmail.com>
2022-06-09 20:36:35 -0400

    pass

--- src/Board.mjs
+++ src/Board.mjs
@@ -6,21 +6,23 @@ export class Board {
   fallingBlock;
   fallingBlockLocation = 0;
 
-  drop(block){ this.falling = true; this.fallingBlock = block.color;}
+  drop(block){if(this.falling === true){throw("already falling");} this.falling = true; this.fallingBlock = block.color;}
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    console.log(width+'-'+height);
   }
  
   tick(){this.fallingBlockLocation++;}
 
   displaying(){
+    if(this.display != ''){this.display = '';}
     for (let i = 0; i < this.height; i++) {
       for(let i2 = 0; i2 < this.width; i2++){
        this.falling && i2 == 1 && i== 0 && this.fallingBlockLocation == i ? this.display += this.fallingBlock: 
         this.falling && i2 == 1 && i == 1 && this.fallingBlockLocation == i ? this.display += this.fallingBlock : 
-          this.falling && i2 == 2 && i == 2 && this.fallingBlockLocation == i ? this.display += this.fallingBlock : this.display += '.';
+          this.falling && i2 == 1 && i == 2 && this.fallingBlockLocation == i ? this.display += this.fallingBlock : this.display += '.';
       }
     this.display = this.display+'\n';}
     return this.display;
