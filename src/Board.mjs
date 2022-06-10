22f83dbf831544b9ae969b344a1f3e4d8cf4db86
fil7er <fil7er@gmail.com>
2022-06-09 20:03:41 -0400

    finally

--- src/Board.mjs
+++ src/Board.mjs
@@ -12,17 +12,21 @@ export class Board {
     this.width = width;
     this.height = height;
   }
+ 
+  tick(){this.fallingBlockLocation++;}
 
-  tick(){this.fallingBlockLocation == 1;}
-
-  toString() {
+  displaying(){
     for (let i = 0; i < this.height; i++) {
       for(let i2 = 0; i2 < this.width; i2++){
        this.falling && i2 == 1 && i== 0 && this.fallingBlockLocation == i ? this.display += this.fallingBlock: 
-        this.falling && i2 == 2 && i == 1 && this.fallingBlockLocation == i ? this.display += this.fallingBlock : 
-          this.falling && i2 == 2 && i == 2 && this.fallingBlockLocation == i ? this.display += this.fallingBlock : this.display += ',';
+        this.falling && i2 == 1 && i == 1 && this.fallingBlockLocation == i ? this.display += this.fallingBlock : 
+          this.falling && i2 == 2 && i == 2 && this.fallingBlockLocation == i ? this.display += this.fallingBlock : this.display += '.';
       }
     this.display = this.display+'\n';}
     return this.display;
+  }
+
+  toString() {
+    return this.displaying().toString();
   }
 }
